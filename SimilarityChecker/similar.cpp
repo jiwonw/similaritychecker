fb75f98f46a21852934a038197b84245700178d0
j1.woo <j1.woo@samsung.com>
2024-07-04 08:45:50 +0900

    partial score added

--- SimilarityChecker/similar.cpp
+++ SimilarityChecker/similar.cpp
@@ -6,12 +6,6 @@ using namespace std;
 class SimilarityChecker
 {
 public:
-	int getSimilarity(string input1, string input2)
-	{
-		getLengthScore(input1, input2);
-		getAppearenceScore(input1, input2);
-		return 100;
-	}
 	int getLengthScore(string a, string b) {
 		int lena = a.size();
 		int lenb = b.size();
@@ -21,10 +15,6 @@ public:
 			return 0;
 		return MAX_LEN_SCORE*(base-gap)/base;
 	}
-	int getAppearenceScore(string a, string b) {
-		return MAX_APR_SCORE;
-	}
 private:
 	const int MAX_LEN_SCORE = 60;
-	const int MAX_APR_SCORE = 40;
 };
--- SimilarityChecker_test/test.cpp
+++ SimilarityChecker_test/test.cpp
@@ -1,15 +1,33 @@
 #include "pch.h"
 #include "../SimilarityChecker/similar.cpp"
 
-TEST(TestSimilarityChecker, SameString000) {
+TEST(TestSimilarityChecker, GetLength000) {
 	SimilarityChecker checker;
-	int actual = checker.getSimilarity("ABC","ABC");
-	int expected = 100;
+	int actual = checker.getLengthScore("ABC","ABC");
+	int expected = 60;
 	EXPECT_EQ(actual, expected);
 }
-TEST(TestSimilarityChecker, SameString001) {
+TEST(TestSimilarityChecker, GetLength001) {
 	SimilarityChecker checker;
-	int actual = checker.getSimilarity("CAFEBADA","CAFEBADA");
-	int expected = 100;
+	int actual = checker.getLengthScore("CAFEBADA","CAFEBADA");
+	int expected = 60;
+	EXPECT_EQ(actual, expected);
+}
+TEST(TestSimilarityChecker, GetLength002) {
+	SimilarityChecker checker;
+	int actual = checker.getLengthScore("ADA","CAFEBADA");
+	int expected = 0;
+	EXPECT_EQ(actual, expected);
+}
+TEST(TestSimilarityChecker, GetLength003) {
+	SimilarityChecker checker;
+	int actual = checker.getLengthScore("ADAB","CAFEBA");
+	int expected = 30;
+	EXPECT_EQ(actual, expected);
+}
+TEST(TestSimilarityChecker, GetLength004) {
+	SimilarityChecker checker;
+	int actual = checker.getLengthScore("ADAB","CAFEB");
+	int expected = 45;
 	EXPECT_EQ(actual, expected);
 }
