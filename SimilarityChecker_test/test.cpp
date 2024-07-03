824c7d60ca450d22689af85403f094f25a02203a
j1.woo <j1.woo@samsung.com>
2024-07-03 17:08:29 +0900

    tc added

--- SimilarityChecker_test/test.cpp
+++ SimilarityChecker_test/test.cpp
@@ -1,9 +1,15 @@
 #include "pch.h"
 #include "../SimilarityChecker/similar.cpp"
 
-TEST(TestSimilarityChecker, Check) {
+TEST(TestSimilarityChecker, SameString000) {
 	SimilarityChecker checker;
 	int actual = checker.getSimilarity("ABC","ABC");
+	int expected = 100;
+	EXPECT_EQ(actual, expected);
+}
+TEST(TestSimilarityChecker, SameString001) {
+	SimilarityChecker checker;
+	int actual = checker.getSimilarity("CAFEBADA","CAFEBADA");
 	int expected = 100;
 	EXPECT_EQ(actual, expected);
 }
