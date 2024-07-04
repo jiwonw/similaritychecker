d04037cb97f40eac4f94736d15d4bb702d07bf86
j1.woo <j1.woo@samsung.com>
2024-07-04 09:23:50 +0900

    parital score on scoring alphabet appearence

--- SimilarityChecker/similar.cpp
+++ SimilarityChecker/similar.cpp
@@ -1,21 +1,55 @@
 
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class SimilarityChecker
 {
 public:
+
 	int getAppearenceScore(string a, string b) 
 	{
-		for (int i=0;i<a.size() && i<b.size();i++)
-		{
-			if (a[i] != b[i])
-			return 0;
-		}
-		return MAX_APR_SCORE;
+		vector<char> appearA = getAppearList(a);
+		vector<char> appearB = getAppearList(b);
+
+		int cntSame = countSameAlpha(appearA, appearB);
+		int cntTotal = appearA.size() + appearB.size() - cntSame;
+
+		return MAX_APR_SCORE * cntSame / cntTotal;
 	}
+
 private:
 
 	const int MAX_APR_SCORE = 40;
+
+	vector<char> getAppearList(string str)
+	{
+		vector<char> appear = {};
+		sort(str.begin(), str.end());
+		for (const char ch : str)
+			if (appear.size() == 0 || appear[appear.size() - 1] != ch)
+				appear.push_back(ch);
+		return appear;
+	}
+
+	int countSameAlpha(vector<char> appearA, vector<char> appearB)
+	{
+		int cntSame = 0;
+		for (int idxA = 0, idxB = 0; idxA < appearA.size() && idxB < appearB.size(); )
+		{
+			if (appearA[idxA] == appearB[idxB]) {
+				cntSame++;
+				idxA++;
+				idxB++;
+				continue;
+			}
+			else if (appearA[idxA] > appearB[idxB]) {
+				idxB++;
+				continue;
+			}
+			idxA++;
+		}
+		return cntSame;
+	}
 };
--- SimilarityChecker_test/test.cpp
+++ SimilarityChecker_test/test.cpp
@@ -13,3 +13,15 @@ TEST(SimilarityChecker, AlphaTest001) {
 	int expected = 0;
 	EXPECT_EQ(actual,expected);
 }
+TEST(SimilarityChecker, AlphaTest002) {
+	SimilarityChecker checker;
+	int actual = checker.getAppearenceScore("ABCD","AAA");
+	int expected = 10;
+	EXPECT_EQ(actual,expected);
+}
+TEST(SimilarityChecker, AlphaTest003) {
+	SimilarityChecker checker;
+	int actual = checker.getAppearenceScore("AB","BBBAAA");
+	int expected = 40;
+	EXPECT_EQ(actual,expected);
+}
