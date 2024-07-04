5817ac821644c43772fcd6c584cff4aa8fc8f658
j1.woo <j1.woo@samsung.com>
2024-07-04 09:54:11 +0900

    fix getLengthScore() as code review comment

--- SimilarityChecker/similar.cpp
+++ SimilarityChecker/similar.cpp
@@ -7,14 +7,18 @@ class SimilarityChecker
 {
 public:
 	int getLengthScore(string a, string b) {
-		int lena = a.size();
-		int lenb = b.size();
-		int base = (lenb > lena ? lena : lenb); // Select short len as base
-		int gap = (lenb > lena ? lenb - lena : lena - lenb);
-		if (gap >= base)
+		int length_a = a.size();
+		int length_b = b.size();
+
+		int length_short = (length_b > length_a ? length_a : length_b); // Select short len as base
+		int length_gap = (length_b > length_a ? length_b - length_a : length_a - length_b);
+
+		if (length_gap >= length_short)
 			return 0;
-		return MAX_LEN_SCORE*(base-gap)/base;
+
+		return MAX_LEN_SCORE*(length_short-length_gap)/length_short;
 	}
+
 private:
 	const int MAX_LEN_SCORE = 60;
 };
